745471eadd9c803e9b5d33d66c0b49de7ec0783b
dormidosan <metahuargen@gmail.com>
2025-06-26 22:49:53 +0100

    redirects

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
   images: {
     //domains: ["imagenes1.com", "imagenes2.com", "imagenes3.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/contact", // Old URL
+        destination: "/contacto", // New URL
+        permanent: true, // true = 308 (permanent), works like 301 for SEO
+      },
+    ];
+  },
 };
 
 export default nextConfig;
